fd55ed1a115e35183d3e94457e8631e6b1f4713f
Olivier <olersteau@gmail.com>
2025-08-25 17:21:31 +0200

    Create handler use case

---
+++ source/src/Cart/Application/Handlers/GetCart/GetCartHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Cart\Application\Handlers\GetCart;
+
+use App\Cart\Application\DTO\GetCartDTO;
+
+final class GetCartHandler
+{
+    public function __invoke(GetCartDTO $dto)
+    {
+        //TODO
+    }
+}
---
+++ source/src/Cart/Application/Handlers/RemoveItemFromCart/RemoveItemFromCartHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Cart\Application\Handlers\RemoveItemFromCart;
+
+use App\Cart\Application\DTO\RemoveItemFromCartDTO;
+
+final class RemoveItemFromCartHandler
+{
+
+    public function __invoke(RemoveItemFromCartDTO $dto): void
+    {
+        //TODO
+    }
+}
