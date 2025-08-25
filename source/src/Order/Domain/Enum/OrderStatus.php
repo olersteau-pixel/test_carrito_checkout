6abc0750a46d491018fa66781a0455d3316e78af
Olivier <olersteau@gmail.com>
2025-08-25 18:38:53 +0200

    Create Order Status enum

---
+++ source/src/Order/Domain/Enum/OrderStatus.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Order\Domain\Enum;
+
+enum OrderStatus: string
+{
+    case PENDING = 'pending';
+    case CONFIRMED = 'confirmed';
+    case SHIPPED = 'shipped';
+    case DELIVERED = 'delivered';
+    case CANCELLED = 'cancelled';
+}
